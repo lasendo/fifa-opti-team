93be51055a120e7ec069323752e122a91afc2b33
lasendo <lasaiker@fastmail.com>
2021-06-03 22:09:27 +0200

    Split to main and module, use Path()

--- fifaOPTI.py
+++ fifaOPTI.py
@@ -34,35 +34,8 @@ import statistics
 import numpy as np
 from ortools.linear_solver import pywraplp # optimization module
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# ------------------------------------------------------------------------------------
-# 2- DATA LOADING
-# ------------------------------------------------------------------------------------
-# TODO: if possible, read data directly from the web
-path_fifa = r'C:/Users/win10/Documents/STRATHCLYDE\\3- Big Data Fundamentals\\1- Assignment\\fifa19\\'
-name_fifa = "data_fifa.csv"
-fifa_original = pd.read_csv(path_fifa + name_fifa)
-fifa = fifa_original.copy()
-# Indicate path where pdf figures should be created
-path_out = r'C:/Users/win10/PycharmProjects/BigDataFundamentals/Project 1/Output/'
-path_opt = path_out + r'Optimization/'
 
 
-# ------------------------------------------------------------------------------------
-# 4- CLEANING
-# ------------------------------------------------------------------------------------
-# > Clean value column price: Charater M and K to million and thousand euros
-def clean_value(value_string):
-    if value_string[-1] == "M": # Million
-        return float(value_string[0:-1]) * 10 ** 6
-    elif value_string[-1] == "K": # Thousand
-        return float(value_string[0:-1]) * 10 ** 3
-    else:
-        return float(value_string)
-
-fifa["Value_clean"] = fifa.Value.map(lambda x: clean_value(re.sub(r'€', '', x)))
-fifa["Wage_clean"] = fifa.Wage.map(lambda x: clean_value(re.sub(r'€', '', x)))
 # -------------------------------------------------------------------------------
 # MIXED INTEGER LINEAR PROGRAMMING (MILP) Optimization
 # -------------------------------------------------------------------------------
@@ -343,87 +316,6 @@ def investment_outcome(start=275000, end=10**8, num=5, goodness_score="Special",
                  alpha=0.5)  # add transparency
 
 
-# -------------------------------------------------------------------------------
-# MAIN
-# -------------------------------------------------------------------------------
-# Calculate optimal team with:
-#       + *score* as Special/Overall attribute
-#       + total_budget
-#       + formation
-# Example:
-#    -> print(optimal_team(total_budget=10**6, goodness_score="Special", formation="1442"))
 
-# > Plot mean optimal score for different total budgets
-# >> With Special
-investment_outcome(num=40,goodness_score="Special", formation="1442",lines={"color":"mediumseagreen","lty":"-"})
-investment_outcome(num=40,goodness_score="Special", formation="1433",lines={"color":"mediumturquoise","lty":"-"})
-plt.title("Investment vs. Optimal Team's mean Special")
-plt.xlabel("Total Budget (M€)")
-plt.ylabel("Mean Special")
-plt.ylim(1150,2300)
-plt.legend(loc='lower right')
-plt.savefig(path_opt+"InvestmentEvolutionSpecial.pdf")
-plt.close()
-# >> With Overall
-investment_outcome(num=40,goodness_score="Overall", formation="1442",lines={"color":"mediumseagreen","lty":"-"})
-investment_outcome(num=40,goodness_score="Overall", formation="1433",lines={"color":"mediumturquoise","lty":"-"})
-plt.title("Investment vs. Optimal Team's mean Overall")
-plt.xlabel("Total Budget (M€)")
-plt.ylabel("Mean Overall")
-plt.ylim(50,90)
-plt.legend(loc='lower right')
-plt.savefig(path_opt+"InvestmentEvolutionOverall.pdf")
-plt.close()
-# III) Reference of clubs' scores
-#  MEAN SCORES OF TEAMS ARE HELPFUL AS REFERENCES
-#     Calculate mean scores of a set of FEW well known clubs:
-#        + With default formation
-#        + With default *score*=Special
-# Example for a small set:
-#  > few_club_list = ["FC Barcelona", "Real Madrid", "Manchester City", "Manchester United"]
-#  > for cb in few_club_list:
-#  > print(cb, club_score(cb))
-# -----------------------------------------
-# > Calculate mean scores of ALL clubs:
-#      + With different formations
-#      + With different *scores*={Special, Overall}
-
-club_list = fifa.Club.unique()
-special_club1433 = {}
-special_club1442 = {}
-overall_club1433 = {}
-overall_club1442 = {}
-for cb in club_list:
-    try:
-        special_club1433[cb] = club_score(cb, formation="1433", score="Special")[0]
-        special_club1442[cb] = club_score(cb, formation="1442", score="Special")[0]
-        overall_club1433[cb] = club_score(cb, formation="1433", score="Overall")[0]
-        overall_club1442[cb] = club_score(cb, formation="1442", score="Overall")[0]
-    except:
-        print("There is not such club in the dataset")
-
-special_club_pd = pd.DataFrame(data={"club_list": list(special_club1433.keys()),
-                                     "1433":list(special_club1433.values()),
-                                     "1442": list(special_club1442.values())})
-overall_club_pd = pd.DataFrame(data={"club_list": list(overall_club1433.keys()),
-                                     "1433":list(overall_club1433.values()),
-                                     "1442": list(overall_club1442.values())})
-# > Plot distributions
-# >>> Special
-special_club_pd.boxplot()
-plt.title("Distribution of mean Special for the 651 clubs")
-plt.ylim(1150, 2300)
-plt.xlabel("Formation")
-plt.ylabel("mean Special of team")
-plt.savefig(path_opt+"SpecialMeanBoxplot.pdf")
-plt.close()
-# >>> Overall
-overall_club_pd.boxplot()
-plt.title("Distribution of mean Overall for the 651 clubs")
-plt.xlabel("Formation")
-plt.ylabel("mean Overall of team")
-plt.ylim(50, 90)
-plt.savefig(path_opt+"OverallMeanBoxplot.pdf")
-plt.close()
 
 
